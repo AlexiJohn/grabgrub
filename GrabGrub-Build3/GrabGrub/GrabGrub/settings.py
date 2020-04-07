f200cb5380510b49c902bbc6465383ccb7a7e16f
Alexis Eusebio <alexis.banares.eusebio@gmail.com>
2020-04-07 15:10:29 +0800

    Changed system settings

--- GrabGrub-Build3/GrabGrub/GrabGrub/settings.py
+++ GrabGrub-Build3/GrabGrub/GrabGrub/settings.py
@@ -10,14 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
-
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
